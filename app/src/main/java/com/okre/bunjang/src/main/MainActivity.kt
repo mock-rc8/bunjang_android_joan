1875885381eef7356501c57846a17a957fc7f549
okrecords <97423205+okrecords@users.noreply.github.com>
2022-08-20 23:22:07 +0900

    bottom_nav

---
+++ app/src/main/java/com/okre/bunjang/src/main/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.okre.bunjang.src.main
+
+import android.os.Bundle
+import com.okre.bunjang.R
+import com.okre.bunjang.config.BaseActivity
+import com.okre.bunjang.databinding.ActivityMainBinding
+import com.okre.bunjang.src.main.home.HomeFragment
+import com.okre.bunjang.src.main.lightningTalk.LightningTalkFragment
+import com.okre.bunjang.src.main.my.MyFragment
+import com.okre.bunjang.src.main.register.RegisterFragment
+import com.okre.bunjang.src.main.search.SearchFragment
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportFragmentManager.beginTransaction().replace(R.id.main_frame_layout, HomeFragment()).commitAllowingStateLoss()
+
+        binding.mainBtmNav.run {
+            setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.menu_main_btm_nav_home -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_frame_layout, HomeFragment())
+                            .commitAllowingStateLoss()
+                    }
+                    R.id.menu_main_btm_nav_search -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_frame_layout, SearchFragment())
+                            .commitAllowingStateLoss()
+                    }
+                    R.id.menu_main_btm_nav_register -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_frame_layout, RegisterFragment())
+                            .commitAllowingStateLoss()
+                    }
+                    R.id.menu_main_btm_nav_lightning_talk -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_frame_layout, LightningTalkFragment())
+                            .commitAllowingStateLoss()
+                    }
+                    R.id.menu_main_btm_nav_my -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_frame_layout, MyFragment())
+                            .commitAllowingStateLoss()
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.menu_main_btm_nav_home
+        }
+    }
+}
