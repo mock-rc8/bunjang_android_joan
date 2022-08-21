336de5450209728fffacc0977532da05a5f8404c
okrecords <97423205+okrecords@users.noreply.github.com>
2022-08-21 23:18:42 +0900

    splash

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+# Bunjang
 
+## 8월 20일 (토)
+- 기획서 작성
+- 홈화면 BottomNavigationView, Fragment
+
+## 8월 21일 (일)
+- 스플래쉬 화면
+- 로그인 화면
--- app/src/main/java/com/okre/bunjang/src/splash/SplashActivity.kt
+++ app/src/main/java/com/okre/bunjang/src/splash/SplashActivity.kt
@@ -4,16 +4,25 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AnimationUtils
+import com.okre.bunjang.R
 import com.okre.bunjang.config.BaseActivity
 import com.okre.bunjang.databinding.ActivitySplashBinding
+import com.okre.bunjang.src.login.LoginActivity
 import com.okre.bunjang.src.main.MainActivity
 
 class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val animation = AnimationUtils.loadAnimation(this, R.anim.scale)
+        binding.splashImage.startAnimation(animation)
+
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+            // 로그인 o
+            //startActivity(Intent(this, MainActivity::class.java))
+            // 로그인 x
+            startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }, 1500)
     }
